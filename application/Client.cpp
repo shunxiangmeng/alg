6257c4ae9655eae68517e75e41b8b289c1eaaf68
shunxiangmeng <shunxiangmeng@gmail.com>
2024-06-17 03:10:01 +0800

    debug

--- application/Client.cpp
+++ application/Client.cpp
@@ -23,7 +23,7 @@ bool Client::initAlg() {
         errorf("alg sdk init failed\n");
         return false;
     }
-    infof("000\n");
+
     ulu_face::SetDefaultPixelFormat(ulu_face::EPIX_FMT_RGB888);
     ulu_face::SetTrackMaxAge(7);
     ulu_face::SetTrackTentativeAge(3);
@@ -36,7 +36,7 @@ bool Client::initAlg() {
         return false;
     }
 
-    person_->SetEScene(ulu_face::ES_CommonStore);
+    //person_->SetEScene(ulu_face::ES_GE60DegreeObliqueStore);
     person_->SetPersonDetectAlgorithm(ulu_face::E_PDA_2); 
 
     std::string detect_model_full_path = "/app/fs/models/detect/FaceDetect_bosideng.rknn";
@@ -84,7 +84,7 @@ bool Client::initAlg() {
         return false;
     }
 
-    std::string attr_model_full_path = "/app/fs/models/feature_extract/FeatureExtractRS.rknn";
+    std::string attr_model_full_path = "/app/fs/models/Attrib/GenderAge.rknn";
     size_t attr_model_size = 0;
     std::shared_ptr<unsigned char> model_data5 = read_model_2_buff(attr_model_full_path.c_str(), attr_model_size);
     tracef("attr_model_size = %d\n", attr_model_size);
@@ -99,9 +99,9 @@ bool Client::initAlg() {
     person_->SetMinHitTimes(1);
     person_->SetMinLifeSeconds(0);
     person_->SetMaxMergeSeconds(0);
-    person_->SetRegionIOU(0.8);
+    person_->SetRegionIOU(0);
     person_->SetMinMoveDistance(0);
-    person_->SetFaceMaxMergeThreshould(0.85);
+    //person_->SetFaceMaxMergeThreshould(0.85);
 
     person_->SetEScene(ulu_face::ES_CommonStore);
 
@@ -114,6 +114,7 @@ bool Client::initAlg() {
 
 void Client::run() {
     infof("start oac client test thread\n");
+    int32_t count = 0;
     while (running()) {
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
@@ -122,6 +123,9 @@ void Client::run() {
         infof("oac client use image index:%d, pts:%lld, wxh[%dx%d], size:%d, data:%p\n", 
             image.index, image.timestamp, image.width, image.height, image.size, image.data);
 
+        ulu_face::STimestamp tv;
+        ulu_face::GetCurrentTimestamp(tv);
+
         ulu_face::SULUImage cv_img;
         cv_img.pixel_format = ulu_face::EPIX_FMT_RGB888;
         cv_img.width = image.width;
@@ -129,18 +133,44 @@ void Client::run() {
         cv_img.vir_addrs[0]= (ulu_face::ulu_uchat_t*)image.data;
         cv_img.data_sizes[0] = image.size;
 
+        cv_img.time_stamp.tv_sec = tv.tv_sec;
+        cv_img.time_stamp.tv_usec = tv.tv_usec;
+
         std::vector<ulu_face::SPersonInfo> out_current_info;
 
+        /*if (count < 5) {
+            std::string filename = "alg" + std::to_string(count++) + ".rgb";
+            FILE *fp = fopen(filename.data(), "wb");
+            if (fp) {
+                fwrite(image.data, 1, image.size, fp);
+                fclose(fp);
+            }
+        }*/
+
         tracef("alg update+++\n");
-        person_->Update(cv_img, out_current_info);
-        tracef("alg update---\n");
+        int ret = person_->Update(cv_img, out_current_info);
+        tracef("alg update--- ret:%d\n", ret);
         tracef("out_current_info.size():%d\n", out_current_info.size());
-        
-        
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
         oac_client_->releaseImageFrame(image);
 
-    }
 
+        if (count++ % 2 == 0) {
+            oac::CurrentDetectResult result;
+            result.timestamp = image.timestamp;
+            for (int32_t i = 0; i < 5; i++) {
+                oac::Target target;
+                target.type = (i % 2) == 0 ? oac::E_TargetType_face : oac::E_TargetType_body;
+                target.id = i;
+                target.rect.x = 0.1 + i * 0.1,
+                target.rect.y = 0.1 + i * 0.05;
+                target.rect.w = 0.2;
+                target.rect.h = 0.15;
+                result.targets.push_back(target);
+            }
+            oac_client_->pushCurrentDetectTarget(result);
+        }
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
 }
